d17343c4178cacb913f90ccc0a5e1cde59b811f2
agustinyuse <agustinyuse@gmail.com>
2022-08-06 19:15:17 -0300

    first commit

---
+++ rentapp.backend/rentapp.backend/Controllers/PropertyController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace rentapp.backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PropertyController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "TEST1", "TEST2" };
+        }
+    }
+}
